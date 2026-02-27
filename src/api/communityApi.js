42c458f57fed1d90d47ac2d5da8540407f083c8d
wmdrkd <wmdrkd02@gmail.com>
2026-02-27 17:42:23 +0900

    게시판 댓글 임시 커밋

--- src/api/communityApi.js
+++ src/api/communityApi.js
@@ -9,3 +9,8 @@ export const getPost = async (postId) => {
   const response = await axiosInstance.get(`/detail/${postId}`);
   return response.data; // 받은 객체에서 json만 꺼내온 것
 };
+
+export const getComments = async (postId) => {
+  const response = await axiosInstance.get(`/comment/list/${postId}`);
+  return response.data; // 받은 객체에서 json만 꺼내온 것
+};
--- src/pages/community/QnaDetail.jsx
+++ src/pages/community/QnaDetail.jsx
@@ -1,20 +1,21 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getPost } from '../../api/communityApi';
+import CommentList from '../../components/community/CommentList';
 
 const QnaDetail = () => {
   
   const { postId } = useParams(); // URL에서 게시물 ID 가져오기
   const [qnaDetail, setQnaDetail] = useState(null);
   
-    useEffect(() => {
-     getPost(postId).then(data => {
-      console.log(data);
-      setQnaDetail(data)
-    });
-    }, [postId]);
+  useEffect(() => {
+  getPost(postId).then(data => {
+    console.log(data);
+    setQnaDetail(data)
+  });
+  }, [postId]);
 
-    if (!qnaDetail) return <div>Loading...</div>;
+  if (!qnaDetail) return <div>Loading...</div>;
   
   return (<>
   
@@ -24,6 +25,8 @@ const QnaDetail = () => {
     <p>작성일: {qnaDetail.createdAt}</p>
     <p>조회수: {qnaDetail.viewCnt}</p>
     <p>{qnaDetail.content}</p>
+    
+    <CommentList postId={postId} />
   </>); 
 }
 export default QnaDetail; 
