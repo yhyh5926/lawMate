8386390b319c30a6403f73dc3b894e30fd86e1bf
yhyh5926 <yhyh5926@naver.com>
2026-03-10 14:35:17 +0900

    baseURL

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -6,7 +6,7 @@ import axios from "axios";
 import { getToken } from "../utils/tokenUtil.js";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8081/api", // 스프링 부트 서버 주소
+  baseURL: "http://localhost:8080/api", // 스프링 부트 서버 주소
   headers: {
     "Content-Type": "application/json",
   },
